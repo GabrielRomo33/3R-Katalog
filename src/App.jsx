e1d5a5f215f9a591903d58c58eee5539e422d224
GabrielRomo33 <rromo@grand.com.mx>
2023-06-20 17:02:51 -0600

    Respaldo 20/06/23 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import CardOption from './CardOption'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <div>
+    <div className='tittle-container'>
+      <img src="./img/casco.png" alt="" className='casco'/>
       <h1 className='tittle'>Romo Bienes Raices y Avaluos</h1>
     </div>
     <div className='techo'>
      <img src="./img/techoimg.jpg" alt="Home" className='techoMenu' />
     </div>
     <div className='menu'>
-      <CardOption />
-      <CardOption />
-      <CardOption />
-      <CardOption />
+      <div className='option'><CardOption /></div>
+      <div className='option'><CardOption /></div>
+      <div className='option'><CardOption /></div>  
+      <div className='option'><CardOption /></div>
     </div>
     </>
   )
